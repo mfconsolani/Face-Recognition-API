184c7bf83982119ab6de564cad0d0cab72c7d7d2
Mauro <mfconsolani@outlook.com>
2020-10-06 12:06:17 +0200

    updated code

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3001, () => {
-    console.log('app is running in port 3001');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running in port ${process.env.PORT}`);
 });
 
 
